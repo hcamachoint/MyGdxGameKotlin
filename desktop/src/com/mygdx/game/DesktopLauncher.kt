25a83c108109df0afe31e8e0c285b871cf774374
Hugo Camacho <hcamacho.int@gmail.com>
2023-01-27 22:41:55 -0400

    first commit

---
+++ desktop/src/com/mygdx/game/DesktopLauncher.kt
@@ -0,0 +1,8 @@
+package com.mygdx.game
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+fun main(args: Array<String>) {
+    Lwjgl3Application(MyGdxGame(), Lwjgl3ApplicationConfiguration())
+}
